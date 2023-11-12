b220694d77444f7fc29d2dd8bf1ba0bf2a9387c7
Emily Rosenberg <earosenberg@gmail.com>
2023-11-12 17:39:17 +0100

    Add files with board images and secret words

--- run.py
+++ run.py
@@ -2,6 +2,9 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import random
+
+
 def start():
     """
     Welcomes the player by name, shows the rules
@@ -18,9 +21,12 @@ def start():
     
     print(f"\nHi {name}! To play, guess the letters in the mystery word.\nYou get 5 incorrect guesses before Snowy melts away...\n")
     start_game = input("Are you ready? Press Enter to start the game!\n")
+    # Does this need validation?
 
 def start_game():
     print("The game is starting")
+
+
     
 start()
 start_game()
---
+++ words.py
@@ -0,0 +1,25 @@
+words = [
+'fog',
+'shovel',
+'frigid',
+'atmosphere',
+'blizzard',
+'rainbow',
+'ozone',
+'frost',
+'humidity',
+'lightning'
+]
+
+hints = [
+'A cloud on the ground',
+'A hand tool',
+'Intensely cold',
+'What is all around us',
+'A severe winter storm',
+'A beautiful arc',
+'It reflects sunlight back into space',
+'Frozen crystals',
+'Water vapor in the air',
+'An electric flash'
+]
